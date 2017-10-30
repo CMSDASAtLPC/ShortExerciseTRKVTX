1d3fde3967563d56f31ac0333e719d25bd1cc896
Julie Hogan <jmhogan@fnal.gov>
2017-10-30 14:35:18 -0500

    cmsdas eos space

--- exercise1a.py
+++ exercise1a.py
@@ -1,5 +1,5 @@
 from DataFormats.FWLite import Handle, Events
-events = Events("root://cmseos.fnal.gov//store/user/jmanagan/TrackingHATS2017/tracks_and_vertices_DoubleMuon2017C_299370.root")
+events = Events("root://cmseos.fnal.gov//store/user/cmsdas/2018/short_exercises/TrackingVertexing/tracks_and_vertices_DoubleMuon2017C_299370.root")
 tracks = Handle("std::vector<reco::Track>")
 
 i = 0
