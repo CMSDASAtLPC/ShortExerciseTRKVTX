e99aa5e5caf299498104b69b47053f7ff4edfa73
Julie Hogan <jmhogan@fnal.gov>
2017-10-30 14:40:05 -0500

    cmsdas eos space

--- extensions/exercise3d.py
+++ extensions/exercise3d.py
@@ -2,7 +2,7 @@ from math import sqrt
 from DataFormats.FWLite import Handle, Events
 import ROOT
 
-events = Events("root://cmseos.fnal.gov//store/user/jmanagan/TrackingHATS2017/tracks_and_vertices_DoubleMuon2017C_299370.root")
+events = Events("root://cmseos.fnal.gov//store/user/cmsdas/2018/short_exercises/TrackingVertexing/tracks_and_vertices_DoubleMuon2017C_299370.root")
 primaryVertices = Handle("std::vector<reco::Vertex>")
 tracks = Handle("std::vector<reco::Track>")
 beamspot = Handle("reco::Beamspot")
